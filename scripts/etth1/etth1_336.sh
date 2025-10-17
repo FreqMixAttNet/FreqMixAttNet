41357a0245bf79a3b0a7a6e5972705584ebdd438
FreqMixAttNet <534412566@qq.com>
2025-10-17 14:57:15 +0800

    Create etth1_336.sh

---
+++ scripts/etth1/etth1_336.sh
@@ -0,0 +1,64 @@
+
+model_name=FreqMixAttNet
+run_date='test'
+root_path='./data'
+
+seq_len=96
+e_layers=2
+learning_rate=0.02
+d_model=32
+n_heads=4
+d_ff=32
+train_epochs=6
+patience=6
+batch_size=128
+dropout=0.2
+down_sampling_layers=2
+down_sampling_window=2
+freq_weight=8
+alpha=0.7 
+l1l2_alpha=0.035
+aug_weight=0.03
+mix_rate=0.1
+jitter_ratio=0.3
+devices='0'
+decomp_method='wavelet'
+
+python -u run_model.py \
+--gpu 0 \
+--task_name long_term_forecast \
+--is_training 1 \
+--devices $devices \
+--data_path ETTh1.csv \
+--root_path $root_path \
+--model_id $run_date'_ETTh1' \
+--model $model_name \
+--data ETTh1 \
+--features M \
+--seq_len $seq_len \
+--label_len 0 \
+--pred_len 336 \
+--e_layers $e_layers \
+--decomp_method wavelet \
+--enc_in 7 \
+--c_out 7 \
+--des 'Exp' \
+--itr 1 \
+--patch_len 16 \
+--d_model $d_model \
+--n_heads $n_heads \
+--d_ff $d_ff \
+--down_sampling_layers $down_sampling_layers \
+--down_sampling_window $down_sampling_window \
+--levels 3 \
+--freq_weight $freq_weight \
+--alpha $alpha \
+--l1l2_alpha $l1l2_alpha \
+--learning_rate $learning_rate \
+--dropout $dropout \
+--train_epochs $train_epochs \
+--patience $patience \
+--batch_size $batch_size \
+--aug_weight $aug_weight \
+--mix_rate $mix_rate \
+--jitter_ratio $jitter_ratio 
